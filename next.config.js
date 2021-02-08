a6e1b9ffc3343ccbc2828f5ec4380f395e85ef55
Nguyễn Hải Triều <36495153+Czer0C@users.noreply.github.com>
2021-02-08 18:51:16 +0700

    fix url

--- next.config.js
+++ next.config.js
@@ -7,7 +7,8 @@ module.exports = withBundleAnalyzer({
   env: {
     CMS_GHOST_API_URL: 'https://gaftoblog.digitalpress.blog',
     CMS_GHOST_API_KEY: '2a4cfcc3d2ee9943aef20991b9',
-    SITE_URL: 'http://localhost:3000',
+    SITE_URL: 'https://gaftoblog.vercel.app',
+    //SITE_URL: 'http://localhost:3000',
   },
   images: {
     deviceSizes: [320, 500, 680, 1040, 2080, 2048, 3120],
